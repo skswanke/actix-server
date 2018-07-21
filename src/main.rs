bca14334ef5f8edd7b8b0486b1c0f285402b6227
Sam Swanke <skswanke@gmail.com>
2018-07-20 20:03:37 -0400

    add index.html

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
+use std::fs::File;
+use std::io::prelude::*;
+use actix_web::{server, App, HttpRequest, HttpResponse, http::ContentEncoding};
 
-fn index(_req: HttpRequest) -> &'static str {
-    "Hello World!"
+fn index(_req: HttpRequest) -> HttpResponse {
+    let filename = "index.html";
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    HttpResponse::Ok()
+        .content_encoding(ContentEncoding::Gzip)
+        .content_type("text/html")
+        .body(contents)
 }
 
 fn main() {
+    let addr = "127.0.0.1:8000";
+    println!("Server running at: http://{}", addr);
     server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8000")
+        .bind(addr)
         .unwrap()
         .run();
 }
